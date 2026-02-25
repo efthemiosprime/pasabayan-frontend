023c82e9601a8aa7e6e91405792c9d5f8e85132b
Efthemios (Bong) Suyat <bongbox@gmail.com>
2026-02-24 20:12:56 -0500

    new pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'build',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        'privacy-policy': resolve(__dirname, 'privacy-policy/index.html'),
+        'data-delete-instructions': resolve(__dirname, 'data-delete-instructions/index.html'),
+      },
+    },
   }
 })
